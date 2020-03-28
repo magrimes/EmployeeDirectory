b911b1d1ba9e6d21f5dde9adad395098c27114bd
Meredith Grimes <meredithagrimes@gmail.com>
2020-03-28 04:05:06 -0400

    added form to search employee

---
+++ employee-directory/src/components/SearchName/SearchName.js
@@ -0,0 +1,27 @@
+import React, { useContext } from "react";
+import "../styles/SearchName.css";
+import DataAreaContext from "../utils/DataAreaContext";
+
+const SearchName = () => {
+  const context = useContext(DataAreaContext);
+
+  return (
+    <div className="searchbox">
+      <div className="input-group">
+          <div className="input-group-prepend">
+            <span className="input-group-text" id="">
+              Search Employee By Name
+            </span>
+          </div>
+          <input
+          className="formsrch"
+          type="search"
+          placeholder="Name"
+          aria-label="Search"
+          onChange={e => context.handleSearchChange(e)}
+        />
+        </div>
+    </div>
+  );
+}
+export default SearchName;
