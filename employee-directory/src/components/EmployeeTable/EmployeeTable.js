a120d5233f0ac57aee722cc32941d3ed39748b1f
Meredith Grimes <meredithagrimes@gmail.com>
2020-03-28 04:27:02 -0400

    created table, table body, sorting by name fixed, styled table

---
+++ employee-directory/src/components/EmployeeTable/EmployeeTable.js
@@ -0,0 +1,43 @@
+import React, { useContext } from "react";
+
+import EmpBody from "./EmpBody";
+import "../styles/EmpTable.css";
+import DataAreaContext from "../utils/DataAreaContext";
+
+const EmpTable = () => {
+    const context = useContext(DataAreaContext);
+
+    return (
+        <div className="datatable mt-5">
+            <table
+                id="table"
+                className="table table-striped table-hover table-condensed"
+            >
+                <thead>
+                    <tr>
+                        {context.developerState.headings.map(({ name, width }) => {
+                            return (
+                                <th
+                                    className="col"
+                                    key={name}
+                                    style={{ width }}
+                                    onClick={() => {
+                                        context.handleSort(name);
+                                        console.log(name)
+                                    }}
+                                >
+                                    {name}
+                                    <span className="pointer"></span>
+                                </th>
+                            );
+                        })}
+                    </tr>
+                </thead>
+
+                <EmpBody />
+            </table>
+        </div>
+    );
+}
+
+export default EmpTable;
